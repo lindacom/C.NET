d7b398732d748150cccb4db5aaf2baa081a7f36e
lindacom <43495865+lindacom@users.noreply.github.com>
2020-11-06 18:27:12 +0000

    Create PersonModel.cs

---
+++ MVC/projects/listpeople/PersonModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
