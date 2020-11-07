cd4145118c1388aba43e9b329212111ac9a943e4
lindacom <43495865+lindacom@users.noreply.github.com>
2020-11-07 20:37:25 +0000

    Create Dashboard.cs

---
+++ WindowsForm/SearchAddPeople/Dashboard.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        List<Person> people = new List<Person>();
+        public Dashboard()
+        {
+            InitializeComponent();
+            UpdateBinding();
+          
+        }
+
+        private void UpdateBinding()
+        {
+            //refreshes the list box information
+            PeopleFoundListbox.DataSource = people;
+            PeopleFoundListbox.DisplayMember = "FullInfo";
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DataAccess db = new DataAccess();
+
+            // gets value fom the text box
+            people = db.GetPeople(LastNameText.Text);
+        }
+    }
+}
