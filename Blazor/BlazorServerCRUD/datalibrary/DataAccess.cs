44a3010fc2153d94f098e2cd8642a6a937efbc53
lindacom <43495865+lindacom@users.noreply.github.com>
2020-11-08 13:14:29 +0000

    Create DataAccess.cs

---
+++ Blazor/BlazorServerCRUD/datalibrary/DataAccess.cs
@@ -0,0 +1,39 @@
+using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class DataAccess : IDataAccess
+    {
+        // load data - await: wait for this to be done before creating a list
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+           
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+            
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+
+                return rows.ToList();
+            }
+        }
+
+        // save data
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+               return connection.ExecuteAsync(sql, parameters);
+
+
+            }
+        }
+
+    }
+}
