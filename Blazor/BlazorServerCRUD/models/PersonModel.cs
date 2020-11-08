c772315041d5dc89acf183dc706002ce676af52d
lindacom <43495865+lindacom@users.noreply.github.com>
2020-11-08 13:10:37 +0000

    Create PersonModel.cs

---
+++ Blazor/BlazorServerCRUD/models/PersonModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class PersonModel
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
